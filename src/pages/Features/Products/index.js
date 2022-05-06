e06dbc8c624d4a5f40fad0d336a4e98854f375de
Dong Ho V <dong.ho@monstar-lab.com>
2022-05-06 14:58:05 +0700

    Inital commit

--- src/pages/Features/Products/index.js
+++ src/pages/Features/Products/index.js
@@ -1,26 +1,17 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react';
 import { apiUserGetList } from '../../../api/user/user.api';
 
 export default function Products() {
-  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     apiUserGetList().then(e => {
-      setUsers(e.data.data);
+      console.log(e);
     });
-    console.log(process.env.REACT_APP_ENV);
+
+    console.log(process.env.REACT_APP_TEST);
   }, []);
 
-  useEffect(() => {
-    console.log(users);
-  }, [users]);
-
   return (
-    <div>Products
-
-      <ul>
-        {users && users.map(e => <li key={e.id}>{e.email}</li>)}
-      </ul>
-    </div>
+    <div>Products</div>
   )
 }
